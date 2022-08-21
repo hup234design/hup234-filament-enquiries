5a6e66f021596a0033fca37f78f9270a9c4ef5d8
HUP234 Design <37693783+hup234design@users.noreply.github.com>
2022-08-21 07:44:55 +0300

    typo and add facade

---
+++ src/Facades/Hup234FilamentEnquries.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Hup234design\Hup234FilamentEnquiries\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * @see \Hup234design\Hup234FilamentEnquiries\Hup234FilamentEnquiries
+ */
+class Hup234FilamentEnquiries extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'hup234-filament-enquiries';
+    }
+}
